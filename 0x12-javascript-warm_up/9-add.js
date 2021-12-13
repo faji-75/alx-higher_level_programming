e6e78fee092daec61cb91d7ee1407b7eee863697
faji-75 <fajisultan@gmail.com>
2021-12-14 02:19:33 +0300

    javascript warm up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+
+function add(a, b) {
+ if (isNaN(a) || isNaN(b)) {
+    return (NaN);
+  } else {
+    return (parseInt(a) + parseInt(b));
+  }
+}
+
+console.log (add (process.argv[2], process.argv[3]))
