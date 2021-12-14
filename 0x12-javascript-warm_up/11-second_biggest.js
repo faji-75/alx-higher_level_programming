307bd91c75000eabbfe7fce6357d132aa8ba6b93
faji-75 <fajisultan@gmail.com>
2021-12-14 03:05:39 +0300

    javascript warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let nextMax = 0;
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 if (args.length > 1) {
   args.sort();
   nextMax = args[args.length - 2];
